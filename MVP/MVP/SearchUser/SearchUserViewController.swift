26764df468e0302e883bab4a75080ad0acf05967
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-06-25 21:52:36 +0900

    初回コミット

---
+++ MVP/MVP/SearchUser/SearchUserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by home on 2020/06/25.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
