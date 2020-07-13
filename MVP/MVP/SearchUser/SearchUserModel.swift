4d7ee44973f40251752168d654b753e61dc25676
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-13 23:46:06 +0900

    リクエスト処理を追加

--- MVP/MVP/SearchUser/SearchUserModel.swift
+++ MVP/MVP/SearchUser/SearchUserModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import GitHub
 
 protocol SearchUserModelInput {
     func fetchUser(
@@ -15,5 +16,24 @@ protocol SearchUserModelInput {
 }
 
 final class SearchUserModel: SearchUserModelInput {
-    
+    func fetchUser(
+        query: String,
+        completion: @escaping (Result<[User]>) -> ()) {
+        let session = GitHub.Session()
+        let request = SearchUsersRequest(
+            query: query,
+            sort: nil,
+            order: nil,
+            page: nil,
+            perPage: nil)
+        
+        session.send(request) { result in
+            switch result {
+            case .success(let response):
+                completion(.success(response.0.items))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
 }
