7742ac83e28d2e448e330e571e0c6df4e9a460a8
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-16 22:23:15 +0900

    viewクラスを追加

--- MVP/MVP/SearchUser/SearchUserViewController.swift
+++ MVP/MVP/SearchUser/SearchUserViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import GitHub
 
-class ViewController: UIViewController {
+final class SearchUserViewController: UIViewController {
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var tableView: UITableView!
+
+    private var presenter: SearchUserPresenterInput!
+    func inject(presenter: SearchUserPresenterInput) {
+        self.presenter = presenter
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setup()
     }
 
-
+    private func setup() {
+        tableView.estimatedRowHeight = 64
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(UINib(nibName: "UserCell", bundle: nil), forCellReuseIdentifier: "UserCell")
+    }
 }
 
+extension SearchUserViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        presenter.didTapSearchButton(text: searchBar.text)
+    }
+}
+
+extension SearchUserViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        presenter.didSelectRow(at: indexPath)
+    }
+}
+
+extension SearchUserViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfUsers
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as! UserCell
+
+        if let user = presenter.user(forRow: indexPath.row) {
+            cell.configure(user: user)
+        }
+
+        return cell
+    }
+}
+
+extension SearchUserViewController: SearchUserPresenterOutput {
+    func updateUsers(_ users: [User]) {
+        tableView.reloadData()
+    }
+
+    func transitionToUserDetail(userName: String) {
+        let userDetailVC = UIStoryboard(
+            name: "UserDetail",
+            bundle: nil)
+            .instantiateInitialViewController() as! UserDetailViewController
+        let model = UserDetailModel(userName: userName)
+        let presenter = UserDetailPresenter(
+            userName: userName,
+            view: userDetailVC,
+            model: model)
+        userDetailVC.inject(presenter: presenter)
+
+        navigationController?.pushViewController(userDetailVC, animated: true)
+    }
+}
